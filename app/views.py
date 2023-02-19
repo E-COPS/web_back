94a32e74aa9bd5756702db420f9a2063a9c324fc
yunjeong <yj2171063@ewhain.net>
2023-02-19 16:44:09 +0900

    static폴더=build로 라우팅

--- app/views.py
+++ app/views.py
@@ -8,37 +8,3 @@ from django.views import generic
 import os
 
 import mysite.settings
-
-
-#react
-# class ReactAppView(View):
-#     def get(self, request):
-#         try:
-#             with open(os.path.join(str(settings.ROOT_DIR), 'webv2_front', 'build', 'index.html')) as file:
-#                 return HttpResponse(file.read())
-#         except:
-#             return HttpResponse(
-#                 status=501,
-#             )
-def Home(request):
-    return render(request, 'home.html')
-def program(request):
-    return render(request,'Program.html')
-
-def member(request):
-    return render(request, '../static/Member.js')
-
-def activity(request):
-    return render(request, '../static/Activity.js')
-#
-def recruitment(request):
-    return render(request, '../static/Recruitment.js')
-
-class ActivityView(TemplateView):
-    template_name = 'Activity.js'
-
-    def get_context_data(self, **kwargs):
-        return {'context_variable':'value'}
-
-    def my_view(request, path=''):
-        return render('Activity.js', context={'context_variable':'value'})
